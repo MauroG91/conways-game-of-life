8b94d5dd5c61e59f235edcdacaf7c994f5261838
MauroG91 <goyamauro@gmail.com>
2018-07-26 19:30:49 -0300

    Disabled start button if input is empty and alert if input is greater than 30

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ var play = function() {
   var liAlives = Board.getCells();
   Board.updateCells(liAlives);
 }
+
 var init = function() {
   Board.rows = document.getElementById('input').value;
   Board.columns = Board.rows;
@@ -11,7 +12,7 @@ var init = function() {
   Game.start();
 }
 
-deleteItems = function(){
+var deleteItems = function() {
   init();
   visibleItems();
   var parent = document.getElementById('game');
@@ -19,13 +20,33 @@ deleteItems = function(){
   parent.removeChild(child);
 }
 
-visibleItems = function(){
+var visibleItems = function() {
   document.getElementById('play').style.visibility = "visible";
   document.getElementById('next').style.visibility = "visible";
   document.getElementsByTagName('ul')[0].style.visibility = "visible";
 }
 
-window.onload = function(){
+var inputCheck = function() {
+  if(document.getElementById('input').value === '') {
+    document.getElementById('start').disabled = true;
+  }else{
+    if(document.getElementById('input').value <= 30) {
+      document.getElementById('start').disabled = false;
+    }else{
+      alert('The number must be less than or equal to 30');
+      document.getElementById('input').value = 30;
+    }
+  }
+}
+
+var eventListener = function() {
+  console.log('eventListener')
+  document.addEventListener("input", inputCheck);
+}
+
+window.onload = function() {
+  document.getElementById('start').disabled = true;
   var btnStart = document.getElementById('start');
   btnStart.onclick = deleteItems;
+  eventListener();
 }
